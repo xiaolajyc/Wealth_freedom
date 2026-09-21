6bce8e8c9126c652433b14ebafa88a97c20f391b
xiaolajyc <72300443+xiaolajyc@users.noreply.github.com>
2026-09-22 02:25:43 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,9 +3,9 @@ const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/apple-touch-icon.png'
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png'
 ];
 
 self.addEventListener('install', event => {
