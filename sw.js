bf9dc9b6352779a03241a3eed6dfb3b2ca513fd3
xiaolajyc <72300443+xiaolajyc@users.noreply.github.com>
2026-09-24 08:13:20 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='wealth-freedom-pwa-v2.9-clean';
+const CACHE='wealth-freedom-pwa-v3.0';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',event=>{
  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
